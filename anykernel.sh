c9dff1282ab89ecc7c35bec25f64fc168739708a
Kdyr Olzhas <kdiroljas@gmail.com>
2026-06-12 02:42:34 +0500

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Perfline+ Kernel by Olzhas0986
+kernel.string=Perf+ Kernel by Olzhas0986
 do.devicecheck=1
 do.modules=0
 do.systemless=1
