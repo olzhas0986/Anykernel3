ecdc2bb1f07f168d92626f7217fbd3ea992770d3
Kdyr Olzhas <kdiroljas@gmail.com>
2026-06-20 12:04:14 +0500

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Perf+ Kernel by Olzhas0986
+kernel.string=Voidness Kernel by Olzhas0986 (Lineage Based)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
